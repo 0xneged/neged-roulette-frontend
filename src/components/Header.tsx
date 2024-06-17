b780d654a647bcd7a38f189ef587e01b92c4685e
T_DaMER <dpancov64@gmail.com>
2024-06-17 19:45:40 +0300

    sticky header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,12 +1,9 @@
-import Button from 'components/Button'
-import WalletIcon from 'components/icons/WalletIcon'
-import { ConnectButton } from '@rainbow-me/rainbowkit'
 import CustomConnectButton from './CustomConnectButton'
 import HatsCounterButton from './HatsCounterButton'
 
 export default function () {
   return (
-    <div className="px-5 py-3 flex-row flex items-center justify-between">
+    <div className="sticky top-0 z-10 backdrop-blur-md px-5 py-3 flex-row flex items-center justify-between">
       <p className="font-script text-neged text-3xl">negeD Hat</p>
       <div className="flex flex-row gap-x-2">
         <HatsCounterButton />
