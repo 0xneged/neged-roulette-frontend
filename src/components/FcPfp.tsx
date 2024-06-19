b79e3623cde14770daa269c3b20e30e022d2a37e
T_DaMER <dpancov64@gmail.com>
2024-06-19 16:14:37 +0300

    Fix fc-pfp on desktop

--- src/components/FcPfp.tsx
+++ src/components/FcPfp.tsx
@@ -3,7 +3,11 @@ import useFcAccount from 'helpers/useFcAccount'
 export default function () {
   const fcUser = useFcAccount()
 
-  console.log(fcUser)
-
-  return <img src={fcUser ? fcUser.pfp_url : ''} />
+  return (
+    <img
+      className="aspect-square"
+      width={44}
+      src={fcUser ? fcUser.pfp_url : ''}
+    />
+  )
 }
