e17a7c7d4b05c029acdb817d4b896e33b7890154
T_DaMER <dpancov64@gmail.com>
2024-06-21 18:08:40 +0300

    Add og, upd favicon

--- src/helpers/api/farcaster.ts
+++ src/helpers/api/farcaster.ts
@@ -5,7 +5,11 @@ import { FcUser } from 'types/FcUser'
 const backendEndpoint = `${env.VITE_BACKEND_URL}/farcaster/getUser`
 
 export default async function (address?: string) {
-  if (!address) return Promise.resolve()
-  const result = await axios.post<FcUser>(backendEndpoint, { address })
-  return result.data
+  try {
+    if (!address) return Promise.resolve()
+    const result = await axios.post<FcUser>(backendEndpoint, { address })
+    return result.data
+  } catch (e) {
+    console.error("Can't fetch Farcaster user", e)
+  }
 }
--- src/helpers/api/token.ts
+++ src/helpers/api/token.ts
@@ -5,7 +5,11 @@ import { FcUser } from 'types/FcUser'
 const backendEndpoint = `${env.VITE_BACKEND_URL}/token/convert`
 
 export default async function (address: string) {
-  if (!address) return Promise.resolve()
-  const result = await axios.post<FcUser>(backendEndpoint, { address })
-  return result.data
+  try {
+    if (!address) return Promise.resolve()
+    const result = await axios.post<FcUser>(backendEndpoint, { address })
+    return result.data
+  } catch (e) {
+    console.error('Failed to swap tokens', e)
+  }
 }
