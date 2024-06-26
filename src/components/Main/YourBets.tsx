024883e7771805e72fccdacd37cc0e0e0b3e4831
T_DaMER <dpancov64@gmail.com>
2024-06-26 20:40:15 +0300

    listen to wallet changes while betting

--- src/components/Main/YourBets.tsx
+++ src/components/Main/YourBets.tsx
@@ -6,19 +6,20 @@ import socket from 'helpers/api/socket'
 import DashedCard from './DashedCard'
 import HatInCircle from '../icons/HatInCircle'
 import { Deposit } from 'types/Round'
+import BetsProps from 'types/BetsProps'
 
-interface YourBetsProps {
-  deposits: Deposit[]
-  totalDeposits: number
-}
-
-export default function ({ deposits, totalDeposits }: YourBetsProps) {
+export default function ({ deposits, totalDeposits }: BetsProps) {
   const { openConnectModal } = useConnectModal()
   const { address } = useAccount()
   const { isConnected } = useAccount()
   const [betAmount, setBetAmount] = useState(12)
+  const [userDeposit, setUserDeposit] = useState(0)
 
-  const userDeposit = deposits.find(({ address }) => address)?.amount
+  useEffect(() => {
+    setUserDeposit(
+      deposits.find((deposit) => deposit.address === address)?.amount || 0
+    )
+  }, [deposits, address])
 
   const onClick = useCallback(() => {
     if (isConnected && betAmount > 0)
@@ -27,20 +28,20 @@ export default function ({ deposits, totalDeposits }: YourBetsProps) {
     if (openConnectModal) openConnectModal()
   }, [isConnected, openConnectModal, address, betAmount])
 
-  if (!userDeposit)
-    return <BigButton onClick={onClick}>TRY YOUR LUCK</BigButton>
+  if (userDeposit > 0)
+    return (
+      <div className="flex flex-row items-center justify-center gap-x-2">
+        <DashedCard subtitle="your bets">
+          <div className="flex gap-x-2">
+            {userDeposit}
+            <HatInCircle />
+          </div>
+        </DashedCard>
+        <DashedCard orange subtitle="your chance">
+          {Math.ceil(userDeposit / totalDeposits) * 100}%
+        </DashedCard>
+      </div>
+    )
 
-  return (
-    <div className="flex flex-row items-center justify-center gap-x-2">
-      <DashedCard subtitle="your bets">
-        <div className="flex gap-x-2">
-          {userDeposit}
-          <HatInCircle />
-        </div>
-      </DashedCard>
-      <DashedCard orange subtitle="your chance">
-        {Math.ceil(userDeposit / totalDeposits) * 100}%
-      </DashedCard>
-    </div>
-  )
+  return <BigButton onClick={onClick}>TRY YOUR LUCK</BigButton>
 }
--- src/helpers/roundNumber.ts
+++ src/helpers/roundNumber.ts
@@ -1,4 +1,5 @@
 export function roundNumber(num: number) {
+  num = Math.ceil(num * 1000) / 1000
   if (num > 1000) return String(num / 1000) + 'K'
   if (num > 1000000) return String(num / 1000000) + 'KK'
   else return num
