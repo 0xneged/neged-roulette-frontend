24bc455345f6fdc275a8f89db032d1ac84eaeefd
T_DaMER <dpancov64@gmail.com>
2024-06-29 19:11:58 +0300

    Add vercel config, move socket init into header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,8 +1,11 @@
 import { Suspense } from 'preact/compat'
 import CustomConnectButton from './CustomConnectButton'
 import Logo from './Logo'
+import useSocket from 'helpers/useSocket'
 
 export default function () {
+  useSocket()
+
   return (
     <div className="sticky top-0 z-10 backdrop-blur-md px-5 py-3 flex-row flex items-center justify-between">
       <Logo />
