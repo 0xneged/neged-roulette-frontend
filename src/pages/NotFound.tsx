1cf73216892454058281dd8cc79a3d4494d0926d
T_DaMER <dpancov64@gmail.com>
2024-07-02 18:36:36 +0300

    Better not found

--- src/pages/NotFound.tsx
+++ src/pages/NotFound.tsx
@@ -1,12 +1,17 @@
+import { Link } from 'wouter-preact'
+
 export default function () {
   return (
-    <div className="flex justify-center w-full items-center">
-      <a
-        className="text-primary font-bold text-9xl text-center cursor-pointer underline"
+    <div className="flex flex-col justify-center w-full items-center">
+      <Link
+        className="!text-primary-bright font-bold text-9xl text-center"
         href="/"
       >
         404
-      </a>
+      </Link>
+      <Link href="/" className="!underline">
+        Go back 🔄🎩
+      </Link>
     </div>
   )
 }
