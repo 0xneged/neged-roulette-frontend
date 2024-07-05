c148ffb44200f7a4af7010cc1649d3cf29e6c015
T_DaMER <dpancov64@gmail.com>
2024-07-05 18:40:20 +0300

    better username styles"

--- src/components/Username.tsx
+++ src/components/Username.tsx
@@ -9,7 +9,7 @@ export default function ({
   truncate?: boolean | undefined
   limitWidth?: boolean | undefined
 }) {
-  const width = limitWidth ? 'w-16 md:w-20' : 'w-24 md:w-40'
+  const width = limitWidth ? 'max-w-16 md:max-w-20' : 'w-24 md:w-40'
   const styles =
     width + ' hover:underline font-semibold opacity-70 text-xs leading-tight '
   const truncation = truncate ? 'truncate' : 'double-rows-break'
