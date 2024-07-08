7cae5f59085a64a4ffa080566136557320a685f7
T_DaMER <dpancov64@gmail.com>
2024-07-08 17:11:54 +0300

    Round chances that are close to zero

--- src/helpers/numbers/getPercentFromTotal.ts
+++ src/helpers/numbers/getPercentFromTotal.ts
@@ -1,3 +1,6 @@
 export default function (amount: number, total: number) {
-  return ((amount / total) * 100).toFixed(2)
+  const result = ((amount / total) * 100).toFixed(2)
+  if (result === '100.00') return '99.99'
+  if (result === '0.00') return '0.01'
+  return result
 }
