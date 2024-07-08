2b6d13a05bcaf210e649024c4d3dd9268722ad87
T_DaMER <dpancov64@gmail.com>
2024-07-08 17:33:56 +0300

    >99.99

--- src/helpers/numbers/getPercentFromTotal.ts
+++ src/helpers/numbers/getPercentFromTotal.ts
@@ -1,6 +1,6 @@
 export default function (amount: number, total: number) {
   const result = ((amount / total) * 100).toFixed(2)
-  if (result === '100.00') return '99.99'
+  if (result === '100.00') return '>99.99'
   if (result === '0.00') return '0.01'
   return result
 }
