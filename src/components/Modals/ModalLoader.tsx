7ac9ae3c973af3714268202f00adab307d8fd75e
T_DaMER <dpancov64@gmail.com>
2024-07-13 19:28:50 +0300

    White loader

--- src/components/Modals/ModalLoader.tsx
+++ src/components/Modals/ModalLoader.tsx
@@ -2,7 +2,7 @@ import HatIcon from 'components/icons/HatIcon'
 
 export default function () {
   return (
-    <div className="flex items-center justify-center w-full h-full">
+    <div className="flex items-center justify-center w-full h-full text-white">
       <HatIcon rotateAnimation />
     </div>
   )
