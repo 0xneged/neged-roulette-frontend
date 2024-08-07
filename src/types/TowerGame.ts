5e2db52261c8d24fe157d18fa00fae5554aefd0e
T_DaMER <dpancov64@gmail.com>
2024-08-07 23:57:26 +0300

    Update multipliers

--- src/types/TowerGame.ts
+++ src/types/TowerGame.ts
@@ -9,8 +9,10 @@ export const TypeToGuessMax = {
 }
 
 export const TypeToMultipliers = {
-  [TowerType.easy]: [1.4, 2.1, 3, 4.4, 6.5, 9.4, 13.8, 20, 29.2],
-  [TowerType.hard]: [3, 7, 14, 27, 53, 103, 200, 389, 755],
+  [TowerType.easy]: [1.2, 1.4, 2.1, 3, 4.4, 6.5, 9.4, 13.8, 20, 29.2],
+  [TowerType.hard]: [
+    1.96, 3.76, 7.3, 14.16, 27.47, 53.31, 103.42, 200.63, 389.21, 755.12,
+  ],
 }
 
 export const towerHeight = (towerType: TowerType) =>
