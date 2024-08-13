d333a2202d0c2e9b51e73594b640794030f46664
T_DaMER <dpancov64@gmail.com>
2024-08-13 19:40:56 +0300

    Fix logo navigation

--- src/components/Logo.tsx
+++ src/components/Logo.tsx
@@ -1,8 +1,9 @@
+import base from 'base'
 import HashLink from 'components/HashLink'
 
 export default function () {
   return (
-    <HashLink href="/">
+    <HashLink href={base}>
       <p className="font-script text-hat text-3xl hover:hue-rotate-15 transition-all drop-shadow-md">
         negeD Hat
       </p>
